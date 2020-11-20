ac85c6c06695196d4db104838b4c8a488a403fb9
Jon Clare <jonclare@users.noreply.github.com>
2020-11-20 23:19:23 +0000

    More robustness around loading and saving events to/from EventStoreDB. (#21)
    * More robustness around loading and saving events to/from EventStoreDB.
    Also added in a logging component that consumers can hook into.
    
    * Tests for the new functionality added in EventStoreEventsRepository

---
+++ src/DomainLib.Persistence/StreamDeletedException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DomainLib.Persistence
+{
+    [Serializable]
+    public class StreamDeletedException : Exception
+    {
+        public string StreamName { get; }
+
+        public StreamDeletedException(string streamName)
+        {
+            StreamName = streamName;
+        }
+
+        public StreamDeletedException(string streamName, string message) : base(message)
+        {
+            StreamName = streamName;
+        }
+
+        public StreamDeletedException(string streamName, string message, Exception inner) : base(message, inner)
+        {
+            StreamName = streamName;
+        }
+
+        protected StreamDeletedException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+            if (info == null) throw new ArgumentNullException(nameof(info));
+
+            info.AddValue(nameof(StreamName), StreamName);
+
+            base.GetObjectData(info, context);
+        }
+    }
+}
