676f70692e259c948ebb915ba43bf540684f6f65
Jon Clare <jon@jonclare.com>
2021-06-27 16:30:12 +0100

    Add an example ASP.NET Core gRPC service that has a domain-lib based domain model
    Also add service collection extensions to register domain-lib write-side types with ASP.NET Core
    
    (cherry picked from commit 45bced1c746afe7f2edde43483f64399cfb1b085)

---
+++ src/Examples/Shopping.Api/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DomainLib.Aggregates;
+using DomainLib.Persistence;
+using MediatR;
+using Shopping.Domain.Events;
+
+namespace Shopping.Api.CommandHandlers
+{
+    public abstract class CommandHandlerBase<TRequest> : IRequestHandler<TRequest, CommandAcknowledgement> where TRequest : IRequest<CommandAcknowledgement>
+    {
+        protected IAggregateRepository<IDomainEvent> Repository { get; }
+        protected CommandDispatcher<object, IDomainEvent> CommandDispatcher { get; }
+
+        protected CommandHandlerBase(IAggregateRepository<IDomainEvent> repository, CommandDispatcher<object, IDomainEvent> commandDispatcher)
+        {
+            Repository = repository;
+            CommandDispatcher = commandDispatcher;
+        }
+
+        public async Task<CommandAcknowledgement> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await HandleImpl(request, cancellationToken);
+
+                return new CommandAcknowledgement
+                {
+                    Success = true
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new CommandAcknowledgement
+                {
+                    Success = false
+                };
+            }
+        }
+
+        protected abstract Task HandleImpl(TRequest request, CancellationToken cancellationToken);
+    }
+}
