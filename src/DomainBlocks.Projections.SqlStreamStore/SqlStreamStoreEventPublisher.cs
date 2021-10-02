3c649cb326c104182c41f89ff89e6983d8fd6ba8
Jon Clare <jon@jonclare.com>
2021-10-02 12:36:14 +0100

    Add SqlStreamStoreEventPublisher for use in projections

---
+++ src/DomainBlocks.Projections.SqlStreamStore/SqlStreamStoreEventPublisher.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SqlStreamStore;
+using SqlStreamStore.Streams;
+using SqlStreamStore.Subscriptions;
+
+namespace DomainBlocks.Projections.SqlStreamStore
+{
+    public class SqlStreamStoreEventPublisher : IEventPublisher<string>, IDisposable
+    {
+        private readonly IStreamStore _streamStore;
+        private Func<EventNotification<string>, Task> _onEvent;
+        private IAllStreamSubscription _subscription;
+        private readonly SqlStreamStoreDroppedSubscriptionHandler _subscriptionDroppedHandler;
+        private long? _lastProcessedPosition;
+
+        public SqlStreamStoreEventPublisher(IStreamStore streamStore)
+        {
+            _streamStore = streamStore;
+            _subscriptionDroppedHandler = new SqlStreamStoreDroppedSubscriptionHandler(Stop, ReSubscribeAfterDrop);
+        }
+
+        public async Task StartAsync(Func<EventNotification<string>, Task> onEvent)
+        {
+            _onEvent = onEvent ?? throw new ArgumentNullException(nameof(onEvent));
+            await SubscribeToStore(null);
+        }
+
+        public void Stop()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _subscription?.Dispose();
+        }
+
+        private async Task SubscribeToStore(long? subscribePosition)
+        {
+            _subscription = _streamStore.SubscribeToAll(subscribePosition, StreamMessageReceived, SubscriptionDropped,
+                                                        caughtUp =>
+                                                        {
+                                                            if (caughtUp)
+                                                            {
+                                                                // SqlStreamStore doesn't provide us with an async delegate.
+                                                                // To avoid ordering issues, we want to ensure no other
+                                                                // event processing is done until our caught up
+                                                                // notification is published , so wait on the task
+                                                                _onEvent(EventNotification.CaughtUp<string>()).Wait();
+                                                            }
+                                                        });
+            // TODO: allow this to be configured
+            _subscription.MaxCountPerRead = 1000;
+            await _subscription.Started;
+        }
+
+        private async Task StreamMessageReceived(IAllStreamSubscription subscription, StreamMessage streamMessage, CancellationToken cancellationToken)
+        {
+            await SendEventNotification(streamMessage);
+        }
+
+        private void SubscriptionDropped(IAllStreamSubscription subscription, SubscriptionDroppedReason reason, Exception exception)
+        {
+            _subscriptionDroppedHandler.HandleDroppedSubscription(reason, exception);
+        }
+
+        async Task SendEventNotification(StreamMessage message)
+        {
+            var notification = EventNotification.FromEvent(await message.GetJsonData(),
+                                                           message.Type,
+                                                           message.MessageId);
+            await _onEvent(notification);
+            _lastProcessedPosition = message.Position;
+        }
+
+        private async Task ReSubscribeAfterDrop()
+        {
+            await SubscribeToStore(_lastProcessedPosition);
+        }
+    }
+}
