b8a746cfb97925dce619b0aef26511d3bccb00a3
Jon Clare <jon@jonclare.com>
2021-10-02 08:54:07 +0100

    Initial work to add a SqlStreamStore persistence provider

---
+++ src/DomainBlocks.Persistence.SqlStreamStore/SqlStreamStoreEventPersistenceData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using DomainBlocks.Serialization;
+using SqlStreamStore.Streams;
+
+namespace DomainBlocks.Persistence.SqlStreamStore
+{
+    public class SqlStreamStoreEventPersistenceData : IEventPersistenceData<string>
+    {
+        private SqlStreamStoreEventPersistenceData(Guid eventId,
+                                                   string eventName,
+                                                   string eventData,
+                                                   string eventMetadata)
+        {
+            EventId = eventId;
+            EventName = eventName;
+            ContentType = MediaTypeNames.Application.Json;
+            EventData = eventData;
+            EventMetadata = eventMetadata;
+        }
+
+        public Guid EventId { get; }
+        public string EventName { get; }
+        public string ContentType { get; }
+        public string EventData { get; }
+        public string EventMetadata { get; }
+
+        public static async Task<IEventPersistenceData<string>> FromStreamMessage(StreamMessage streamMessage)
+        {
+            return new SqlStreamStoreEventPersistenceData(streamMessage.MessageId,
+                                                          streamMessage.Type,
+                                                          await streamMessage.GetJsonData(),
+                                                          streamMessage.JsonMetadata);
+        }
+    }
+}
---
+++ src/DomainBlocks.Persistence.SqlStreamStore/SqlStreamStoreSnapshotRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DomainBlocks.Persistence.SqlStreamStore
+{
+    public class SqlStreamStoreSnapshotRepository : ISnapshotRepository
+    {
+        public Task SaveSnapshotAsync<TState>(string snapshotKey, long snapshotVersion, TState snapshotState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool isSuccess, Snapshot<TState> snapshot)> TryLoadSnapshotAsync<TState>(string snapshotKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
