a17f37baad825ac3a6e841514d406a3b27aceb28
Jon Clare <jon@jonclare.com>
2021-10-02 08:54:07 +0100

    Initial work to add a SqlStreamStore persistence provider

---
+++ src/DomainBlocks.Persistence.SqlStreamStore/NewStreamMessageExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DomainBlocks.Serialization;
+using SqlStreamStore.Streams;
+
+namespace DomainBlocks.Persistence.SqlStreamStore
+{
+    public static class NewStreamMessageExtensions
+    {
+        public static NewStreamMessage ToNewStreamMessage(this IEventSerializer<string> @eventSerializer, object @event, string eventNameOverride = null, params KeyValuePair<string, string>[] additionalMetadata)
+        {
+            var eventPersistenceData = eventSerializer.GetPersistenceData(@event, eventNameOverride, additionalMetadata);
+            return new NewStreamMessage(eventPersistenceData.EventId,
+                                        eventPersistenceData.EventName,
+                                        eventPersistenceData.EventData,
+                                        eventPersistenceData.EventMetadata);
+        }
+    }
+}
