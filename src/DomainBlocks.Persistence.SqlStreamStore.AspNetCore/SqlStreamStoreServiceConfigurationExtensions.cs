1d1d93275be5369e298d592b6aafcf270f55cef4
Carlos Fernandez <carlosrfa@outlook.com>
2021-12-14 00:03:18 +0100

    Working with postgres

--- src/DomainBlocks.Persistence.SqlStreamStore.AspNetCore/SqlStreamStoreServiceConfigurationExtensions.cs
+++ src/DomainBlocks.Persistence.SqlStreamStore.AspNetCore/SqlStreamStoreServiceConfigurationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using SqlStreamStore;
 
 namespace DomainBlocks.Persistence.SqlStreamStore.AspNetCore
@@ -7,7 +8,7 @@ namespace DomainBlocks.Persistence.SqlStreamStore.AspNetCore
     public static class SqlStreamStoreServiceConfigurationExtensions
     {
         private static readonly object SyncLock = new();
-        private static IStreamStore _sqlStreamStore;
+        private static PostgresStreamStore _sqlStreamStore;
 
         public static IServiceCollection AddPostgresSqlStreamStore(this IServiceCollection services, IConfiguration configuration)
         {
@@ -15,7 +16,7 @@ namespace DomainBlocks.Persistence.SqlStreamStore.AspNetCore
                     .Bind(configuration.GetSection(SqlStreamStoreConnectionOptions.ConfigSection))
                     .ValidateDataAnnotations();
 
-            services.AddSingleton(_ =>
+            services.AddSingleton<IStreamStore>(provider =>
             {
                 if (_sqlStreamStore == null)
                 {
@@ -23,7 +24,11 @@ namespace DomainBlocks.Persistence.SqlStreamStore.AspNetCore
                     {
                         if (_sqlStreamStore == null)
                         {
-                            _sqlStreamStore = new InMemoryStreamStore();
+                            var options = provider.GetRequiredService<IOptions<SqlStreamStoreConnectionOptions>>();
+
+                            var settings = new PostgresStreamStoreSettings(options.Value.ConnectionString);
+                            _sqlStreamStore = new PostgresStreamStore(settings);
+                            _sqlStreamStore.CreateSchemaIfNotExists().Wait();
                         }
                     }
                 }
