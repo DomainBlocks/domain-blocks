985a599b2edfbdfee75d776c87d109465a4be23e
Jon Clare <jon@jonclare.com>
2021-12-15 20:59:20 +0000

    Add SqlStreamStore snapshot repository implementation

--- src/DomainBlocks.Persistence.SqlStreamStore/SqlStreamStoreSnapshotRepository.cs
+++ src/DomainBlocks.Persistence.SqlStreamStore/SqlStreamStoreSnapshotRepository.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using DomainBlocks.Common;
 using DomainBlocks.Serialization;
+using Microsoft.Extensions.Logging;
 using SqlStreamStore;
+using SqlStreamStore.Streams;
 
 namespace DomainBlocks.Persistence.SqlStreamStore
 {
     public class SqlStreamStoreSnapshotRepository : ISnapshotRepository
     {
+        private const string SnapshotVersionMetadataKey = "SnapshotVersion";
+        private const string SnapshotEventName = "Snapshot";
+        private static readonly ILogger<SqlStreamStoreSnapshotRepository> Log = Logger.CreateFor<SqlStreamStoreSnapshotRepository>();
         private readonly IStreamStore _streamStore;
         private readonly IEventSerializer<string> _serializer;
 
@@ -16,15 +23,62 @@ namespace DomainBlocks.Persistence.SqlStreamStore
             _serializer = serializer;
         }
 
-        public Task SaveSnapshotAsync<TState>(string snapshotKey, long snapshotVersion, TState snapshotState)
+        public async Task SaveSnapshotAsync<TState>(string snapshotKey, long snapshotVersion, TState snapshotState)
         {
-            throw new NotImplementedException();
+            if (snapshotKey == null) throw new ArgumentNullException(nameof(snapshotKey));
+            if (snapshotState == null) throw new ArgumentNullException(nameof(snapshotState));
+
+            try
+            {
+                var snapshotData = new[]
+                {
+                    _serializer.ToNewStreamMessage(snapshotState,
+                                                   SnapshotEventName,
+                                                   KeyValuePair.Create(SnapshotVersionMetadataKey, snapshotVersion.ToString()))
+                };
+
+                await _streamStore.AppendToStream(snapshotKey, ExpectedVersion.Any, snapshotData);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex,
+                             "Error when attempting to save snapshot. Stream Name {StreamName}. " +
+                             "Snapshot Version {SnapshotVersion}, Snapshot Type {SnapshotType}",
+                             snapshotKey,
+                             snapshotVersion,
+                             typeof(TState).FullName);
+                throw;
+            }
         }
 
-        public Task<(bool isSuccess, Snapshot<TState> snapshot)> TryLoadSnapshotAsync<TState>(string snapshotKey)
+        public async Task<(bool isSuccess, Snapshot<TState> snapshot)> TryLoadSnapshotAsync<TState>(string snapshotKey)
         {
-            (bool, Snapshot<TState>) result = (false, default);
-            return Task.FromResult(result);
+            try
+            {
+                if (snapshotKey == null) throw new ArgumentNullException(nameof(snapshotKey));
+                var readStreamPage = await _streamStore.ReadStreamBackwards(snapshotKey, global::SqlStreamStore.Streams.StreamVersion.End,  1);
+                var messages = readStreamPage.Messages;
+
+                if (messages is not { Length: 1 })
+                {
+                    return (false, null);
+                }
+
+                var snapshotMessage = messages[0];
+                var snapshotState = _serializer.DeserializeEvent<TState>(await snapshotMessage.GetJsonData(),
+                                                                         snapshotMessage.Type,
+                                                                         typeof(TState));
+
+                var snapshotVersion = long.Parse(_serializer.DeserializeMetadata(snapshotMessage.JsonMetadata)[SnapshotVersionMetadataKey]);
+                return (true, new Snapshot<TState>(snapshotState, snapshotVersion));
+
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex, "Error when attempting to load snapshot. Stream Name: {StreamName}. " +
+                                 "Snapshot Type {SnapshotType}", snapshotKey, typeof(TState).FullName);
+                throw;
+            }
         }
     }
 }
