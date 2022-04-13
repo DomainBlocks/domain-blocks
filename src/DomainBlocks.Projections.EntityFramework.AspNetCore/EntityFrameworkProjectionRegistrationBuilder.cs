be843880e66fcb10ed8ccac5142be682d14205f8
Daniel Smith <daniel_smith@crgl-thirdparty.com>
2022-04-13 19:41:12 +0100

    WIP: wrapping builders to pass DbContext through

--- src/DomainBlocks.Projections.EntityFramework.AspNetCore/EntityFrameworkProjectionRegistrationBuilder.cs
+++ src/DomainBlocks.Projections.EntityFramework.AspNetCore/EntityFrameworkProjectionRegistrationBuilder.cs
@@ -8,13 +8,14 @@ namespace DomainBlocks.Projections.EntityFramework.AspNetCore
     {
         private Action<IServiceProvider, ProjectionRegistryBuilder> _onRegisteringProjections;
 
-        public void WithProjections(Action<ProjectionRegistryBuilder, TDbContext> onRegisteringProjections)
+        public void WithProjections(Action<EntityFrameworkProjectionRegistryBuilder<TDbContext>, TDbContext> onRegisteringProjections)
         {
             _onRegisteringProjections = (provider, builder) =>
             {
                 var dispatcherScope = provider.CreateScope();
                 var dbContext = dispatcherScope.ServiceProvider.GetRequiredService<TDbContext>();
-                onRegisteringProjections(builder, dbContext);
+                var efProjectionRegistryBuilder = new EntityFrameworkProjectionRegistryBuilder<TDbContext>(builder, dbContext);
+                onRegisteringProjections(efProjectionRegistryBuilder, dbContext);
             };
         }
 
@@ -22,5 +23,38 @@ namespace DomainBlocks.Projections.EntityFramework.AspNetCore
         {
             return (provider, builder) => _onRegisteringProjections(provider, builder);
         }
+    }
+
+    public class EntityFrameworkProjectionRegistryBuilder<TDbContext>
+    {
+        private readonly ProjectionRegistryBuilder _inner;
+        private readonly TDbContext _dbContext;
+
+        internal EntityFrameworkProjectionRegistryBuilder(ProjectionRegistryBuilder inner, TDbContext dbContext)
+        {
+            _inner = inner;
+            _dbContext = dbContext;
+        }
+
+        public EntityFrameworkEventProjectionBuilder<TDbContext, TEvent> Event<TEvent>()
+        {
+            return new EntityFrameworkEventProjectionBuilder<TDbContext, TEvent>(_inner.Event<TEvent>(), _dbContext);
+        }
+    }
+
+    public class EntityFrameworkEventProjectionBuilder<TDbContext, TEvent>
+    {
+        private readonly EventProjectionBuilder<TEvent> _inner;
+        private readonly TDbContext _dbContext;
+
+        public EntityFrameworkEventProjectionBuilder(EventProjectionBuilder<TEvent> inner, TDbContext dbContext)
+        {
+            _inner = inner;
+            _dbContext = dbContext;
+        }
+
+        public EntityFrameworkEventProjectionBuilder<TDbContext, TEvent> FromName(string name) => _inner.FromName(name);
+
+        public EntityFrameworkEventProjectionBuilder<TDbContext, TEvent> FromNames(params string[] names) => _inner.FromNames(names);
     }
 }
