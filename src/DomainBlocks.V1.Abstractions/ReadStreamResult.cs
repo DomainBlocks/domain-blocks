3f5895a568594f2f0834cbe9bbc37bab78d449ea
Jon Clare <jon@jonclare.com>
2024-06-28 14:39:28 +0100

    Remove dependency on Stytem.Linq.Async

--- src/DomainBlocks.V1.Abstractions/ReadStreamResult.cs
+++ src/DomainBlocks.V1.Abstractions/ReadStreamResult.cs
@@ -8,12 +8,20 @@ public sealed class ReadStreamResult
         EventRecords = eventRecords;
     }
     
-    public static ReadStreamResult NotFound() => new(ReadStreamStatus.StreamNotFound, 
-        AsyncEnumerable.Empty<StoredEventRecord>());
+    public static ReadStreamResult NotFound() => new(ReadStreamStatus.StreamNotFound, CreateEmptyEventRecords());
     
     public static ReadStreamResult Success(IAsyncEnumerable<StoredEventRecord> eventRecords) =>
         new(ReadStreamStatus.Success, eventRecords);
     
     public ReadStreamStatus Status { get; } 
     public IAsyncEnumerable<StoredEventRecord> EventRecords { get; }
+    
+    // To avoid taking a dependency on System.Linq.Async, we provide an implementation of an empty IAsyncEnumerable.
+    // If we find that we need other things from that assembly in the future, we may revisit this decision.
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    private static async IAsyncEnumerable<StoredEventRecord> CreateEmptyEventRecords()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    {
+        yield break;
+    }
 }
