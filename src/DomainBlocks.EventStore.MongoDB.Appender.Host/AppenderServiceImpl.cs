d57326b41603aec4a323cf8874dc6edac724d552
Daniel Smith <danielmsmith@gmail.com>
2026-01-14 20:46:47 +0000

    Add initial appender API and host (WIP)

---
+++ src/DomainBlocks.EventStore.MongoDB.Appender.Host/AppenderServiceImpl.cs
@@ -0,0 +1,15 @@
+﻿using DomainBlocks.EventStore.Api.Appender.V0;
+using Grpc.Core;
+
+namespace DomainBlocks.EventStore.MongoDB.Appender.Host;
+
+public sealed class AppenderServiceImpl : AppenderService.AppenderServiceBase
+{
+    public override Task<AppendToStreamResponse> AppendToStream(
+        AppendToStreamRequest request,
+        ServerCallContext context)
+    {
+        var response = new AppendToStreamResponse();
+        return Task.FromResult(response);
+    }
+}
---
+++ src/DomainBlocks.EventStore.MongoDB.Appender.Host/Program.cs
@@ -0,0 +1,17 @@
+﻿using DomainBlocks.EventStore.MongoDB.Appender.Host;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(50051, listenOptions => listenOptions.Protocols = HttpProtocols.Http2);
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<AppenderServiceImpl>();
+
+app.Run();
