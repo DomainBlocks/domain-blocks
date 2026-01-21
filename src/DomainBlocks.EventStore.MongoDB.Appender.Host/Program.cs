383aec268371e992b9a3ac161ee88e0c2b9f1dd5
Daniel Smith <danielmsmith@gmail.com>
2026-01-21 16:06:19 +0000

    Add appsettings.json

--- src/DomainBlocks.EventStore.MongoDB.Appender.Host/Program.cs
+++ src/DomainBlocks.EventStore.MongoDB.Appender.Host/Program.cs
@@ -1,18 +1,14 @@
 ﻿using DomainBlocks.EventStore.MongoDB.Appender.Host;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 
-builder.Services.AddSingleton<IMongoClient>(new MongoClient("mongodb://localhost:27017"));
-builder.Services.AddSingleton<EventAppender>();
+var connectionString = builder.Configuration.GetConnectionString("Mongo");
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenLocalhost(50051, listenOptions => listenOptions.Protocols = HttpProtocols.Http2);
-});
+builder.Services.AddSingleton<EventAppender>();
 
 var app = builder.Build();
 
