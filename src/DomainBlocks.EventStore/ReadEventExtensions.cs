3a80607a3537b8a30db5fd9b1258819a2c560d94
Daniel Smith <danielmsmith@gmail.com>
2026-01-25 14:46:47 +0000

    Add doc comment for Unwrap

--- src/DomainBlocks.EventStore/ReadEventExtensions.cs
+++ src/DomainBlocks.EventStore/ReadEventExtensions.cs
@@ -4,12 +4,18 @@ namespace DomainBlocks.EventStore;
 
 public static class ReadEventExtensions
 {
+    /// <summary>
+    /// Unwraps the event objects from a sequence of <see cref="ReadEvent{TEvent}"/> instances.
+    /// </summary>
     public static IEnumerable<TEvent> Unwrap<TEvent>(this IEnumerable<ReadEvent<TEvent>> source)
         where TEvent : notnull
     {
         return source.Select(x => x.Event);
     }
 
+    /// <summary>
+    /// Unwraps the event objects from a sequence of <see cref="ReadEvent{TEvent}"/> instances.
+    /// </summary>
     public static IAsyncEnumerable<TEvent> Unwrap<TEvent>(this IAsyncEnumerable<ReadEvent<TEvent>> source)
         where TEvent : notnull
     {
