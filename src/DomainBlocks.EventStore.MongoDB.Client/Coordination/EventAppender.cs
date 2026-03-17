2554754bce2e1f0d42158b6f2e00ca405c489055
Daniel Smith <danielmsmith@gmail.com>
2026-03-17 08:17:09 +0000

    Use StreamState in EventAppender

--- src/DomainBlocks.EventStore.MongoDB.Client/Coordination/EventAppender.cs
+++ src/DomainBlocks.EventStore.MongoDB.Client/Coordination/EventAppender.cs
@@ -1,4 +1,5 @@
-﻿using DomainBlocks.EventStore.MongoDB.Client.Schema2;
+﻿using DomainBlocks.EventStore.Abstractions;
+using DomainBlocks.EventStore.MongoDB.Client.Schema2;
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
@@ -39,22 +40,21 @@ public sealed class EventAppender(
             if (!batchContext.CommitIds.Add(request.CommitId))
                 continue;
 
-            var streamVersion = batchContext.StreamVersions.GetValueOrDefault(request.StreamId, -1);
+            var streamState = batchContext.StreamStates.GetValueOrDefault(request.StreamId);
+            var streamVersionValue = streamState.IsStreamExists ? streamState.Version.Value.ToInt64() : -1;
 
-            for (var i = 0; i < request.Events.Length; i++)
+            foreach (var @event in request.Events)
             {
                 var position = _nextPosition++;
 
                 var filter = Builders<EventLogEntry>.Filter.Eq(x => x.Position, position) &
                              Builders<EventLogEntry>.Filter.Lt(x => x.Epoch, epoch);
 
-                var @event = request.Events[i];
-
                 var update = Builders<EventLogEntry>.Update
                     .Set(x => x.Position, position)
                     .Set(x => x.Epoch, epoch)
                     .Set(x => x.StreamId, request.StreamId)
-                    .Set(x => x.StreamVersion, ++streamVersion)
+                    .Set(x => x.StreamVersion, ++streamVersionValue)
                     .Set(x => x.CommitId, request.CommitId)
                     .Set(x => x.EventName, @event.EventName)
                     .Set(x => x.EventData, @event.EventData)
@@ -64,7 +64,8 @@ public sealed class EventAppender(
                 models.Add(new UpdateOneModel<EventLogEntry>(filter, update) { IsUpsert = true });
             }
 
-            batchContext.StreamVersions[request.StreamId] = streamVersion;
+            var streamVersion = StreamVersion.FromInt64(streamVersionValue);
+            batchContext.StreamStates[request.StreamId] = StreamState.StreamExists(streamVersion);
         }
 
         if (models.Count == 0)
@@ -117,25 +118,31 @@ public sealed class EventAppender(
                 cancellationToken: cancellationToken)
             .ToListAsync(cancellationToken);
 
-        var streamVersionsTask = eventLog
+        var maxVersionsTask = eventLog
             .Aggregate()
             .Match(Builders<EventLogEntry>.Filter.In(x => x.StreamId, streamIds) & _visibilityFilter)
             .Group(x => x.StreamId, g => new { StreamId = g.Key, MaxVersion = g.Max(x => x.StreamVersion) })
             .ToListAsync(cancellationToken);
 
-        await Task.WhenAll(commitIdsTask, streamVersionsTask).ConfigureAwait(false);
+        await Task.WhenAll(commitIdsTask, maxVersionsTask).ConfigureAwait(false);
 
         var commitIdSet = (await commitIdsTask.ConfigureAwait(false)).ToHashSet();
 
-        var streamVersions = (await streamVersionsTask.ConfigureAwait(false))
-            .ToDictionary(x => x.StreamId, x => x.MaxVersion);
+        var streamStates = (await maxVersionsTask.ConfigureAwait(false))
+            .ToDictionary(
+                x => x.StreamId,
+                x =>
+                {
+                    var version = StreamVersion.FromInt64(x.MaxVersion);
+                    return StreamState.StreamExists(version);
+                });
 
-        return new BatchContext(commitIdSet, streamVersions);
+        return new BatchContext(commitIdSet, streamStates);
     }
 
-    private readonly struct BatchContext(HashSet<Guid> commitIds, Dictionary<string, long> streamVersions)
+    private readonly struct BatchContext(HashSet<Guid> commitIds, Dictionary<string, StreamState> streamStates)
     {
         public HashSet<Guid> CommitIds { get; } = commitIds;
-        public Dictionary<string, long> StreamVersions { get; } = streamVersions;
+        public Dictionary<string, StreamState> StreamStates { get; } = streamStates;
     }
 }
