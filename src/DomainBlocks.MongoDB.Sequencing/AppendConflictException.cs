6bb9e2e209dfd0ed391717bcd1d45af5d05c7963
Daniel Smith <danielmsmith@gmail.com>
2026-05-09 09:59:12 +0100

    refactor: use sequencing lib

--- src/DomainBlocks.MongoDB.Sequencing/AppendConflictException.cs
+++
@@ -1,7 +0,0 @@
-namespace DomainBlocks.MongoDB.Sequencing;
-
-/// <summary>
-/// Thrown when an append operation fails due to an unresolved conflict.
-/// </summary>
-public class AppendConflictException(string? message = null, Exception? innerException = null) :
-    Exception(message, innerException);
--- src/DomainBlocks.MongoDB.Sequencing/MongoErrorCodes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DomainBlocks.MongoDB.Sequencing;
-
-internal static class MongoErrorCodes
-{
-    public const int DuplicateKey = 11000;
-}
