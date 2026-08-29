6c1a1c2bcf90984a67d160fac80941ef3036a842
Daniel Smith <danielmsmith@gmail.com>
2026-08-29 20:41:10 +0100

    Simplify event transform generic args

--- src/DomainBlocks.EventStore/Transforms/IReadEventTransform.cs
+++ src/DomainBlocks.EventStore/Transforms/IReadEventTransform.cs
@@ -2,13 +2,13 @@ using DomainBlocks.EventStore.Abstractions;
 
 namespace DomainBlocks.EventStore.Transforms;
 
-public interface IReadEventTransform<TEventBase, TStreamId, TStreamPos, TLogPos>
-    where TEventBase : class
+public interface IReadEventTransform<TEvent, TStreamId, TStreamPos, TLogPos>
+    where TEvent : notnull
     where TStreamId : notnull
     where TStreamPos : notnull
     where TLogPos : notnull
 {
     Type SourceEventType { get; }
 
-    IEnumerable<TEventBase> Apply(ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos> sourceEvent);
+    IEnumerable<TEvent> Apply(ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos> sourceEvent);
 }
--- src/DomainBlocks.EventStore/Transforms/ReadEventTransformExtensions.cs
+++ src/DomainBlocks.EventStore/Transforms/ReadEventTransformExtensions.cs
@@ -5,32 +5,32 @@ namespace DomainBlocks.EventStore.Transforms;
 
 public static class ReadEventTransformExtensions
 {
-    public static IAsyncEnumerable<ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos>>
-        Transform<TEventBase, TStreamId, TStreamPos, TLogPos>(
-            this IAsyncEnumerable<ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos>> source,
-            IEnumerable<IReadEventTransform<TEventBase, TStreamId, TStreamPos, TLogPos>> transforms)
-        where TEventBase : class
+    public static IAsyncEnumerable<ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos>>
+        Transform<TEvent, TStreamId, TStreamPos, TLogPos>(
+            this IAsyncEnumerable<ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos>> source,
+            IEnumerable<IReadEventTransform<TEvent, TStreamId, TStreamPos, TLogPos>> transforms)
+        where TEvent : notnull
         where TStreamId : notnull
         where TStreamPos : notnull
         where TLogPos : notnull
     {
-        return new TransformAsyncEnumerable<TEventBase, TStreamId, TStreamPos, TLogPos>(source, transforms);
+        return new TransformAsyncEnumerable<TEvent, TStreamId, TStreamPos, TLogPos>(source, transforms);
     }
 
-    private class TransformAsyncEnumerable<TEventBase, TStreamId, TStreamPos, TLogPos>(
-        IAsyncEnumerable<ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos>> source,
-        IEnumerable<IReadEventTransform<TEventBase, TStreamId, TStreamPos, TLogPos>> transforms) :
-        IAsyncEnumerable<ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos>>
-        where TEventBase : class
+    private class TransformAsyncEnumerable<TEvent, TStreamId, TStreamPos, TLogPos>(
+        IAsyncEnumerable<ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos>> source,
+        IEnumerable<IReadEventTransform<TEvent, TStreamId, TStreamPos, TLogPos>> transforms) :
+        IAsyncEnumerable<ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos>>
+        where TEvent : notnull
         where TStreamId : notnull
         where TStreamPos : notnull
         where TLogPos : notnull
     {
-        public async IAsyncEnumerator<ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos>> GetAsyncEnumerator(
+        public async IAsyncEnumerator<ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos>> GetAsyncEnumerator(
             CancellationToken cancellationToken = default)
         {
             var transformsByType = transforms.ToFrozenDictionary(x => x.SourceEventType);
-            var queue = new Queue<ReadEvent<TEventBase, TStreamId, TStreamPos, TLogPos>>();
+            var queue = new Queue<ReadEvent<TEvent, TStreamId, TStreamPos, TLogPos>>();
 
             await foreach (var sourceEvent in source.WithCancellation(cancellationToken).ConfigureAwait(false))
             {
